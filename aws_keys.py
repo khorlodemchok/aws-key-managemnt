28a9c1e27ffa33a99261332a18435345ec3e5aa3
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2019-06-19 16:08:32 +0200

    local_fp works

--- aws_keys.py
+++ aws_keys.py
@@ -1,7 +1,16 @@
+from Crypto.PublicKey import RSA
+import hashlib
+
+
 def fp_format(in_str):
     chunks = [in_str[i:i+2] for i in range(0, len(in_str), 2)]
     return ":".join(chunks)
 
 
-def local_fingerprint():
-    return "something"
+def local_fingerprint(key_file_obj):
+    key = RSA.import_key(key_file_obj.read())
+    k_der = key.publickey().export_key(format='DER')
+    hash_sum = hashlib.md5(k_der).hexdigest()
+    return fp_format(hash_sum)
+
+    
--- test_aws_keys.py
+++ test_aws_keys.py
@@ -7,4 +7,6 @@ def test_fp_format():
 
 
 def test_local_fingerprint():
-    assert local_fingerprint()
+    proper_fp = "65:8f:82:a0:8f:53:77:ad:63:c8:f7:15:d6:36:69:04"
+    with open('locally-created-key') as key_file_obj:
+        assert local_fingerprint(key_file_obj) == proper_fp
