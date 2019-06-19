7ccd5dd4c2f35b4889c88b68d06b2eb1e64c2bb1
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2019-06-19 15:58:56 +0200

    fp_format works

--- aws_keys.py
+++ aws_keys.py
@@ -1,2 +1,7 @@
+def fp_format(in_str):
+    chunks = [in_str[i:i+2] for i in range(0, len(in_str), 2)]
+    return ":".join(chunks)
+
+
 def local_fingerprint():
     return "something"
--- test_aws_keys.py
+++ test_aws_keys.py
@@ -1,4 +1,9 @@
-from aws_keys import local_fingerprint
+from aws_keys import local_fingerprint, fp_format
+
+def test_fp_format():
+    in_str = "12345"
+    out_str = "12:34:5"
+    assert fp_format(in_str) == out_str
 
 
 def test_local_fingerprint():
