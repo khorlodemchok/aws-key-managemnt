8ba8c54e762287ed8197df0bba9d5d9a71d90375
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2019-06-20 09:35:03 +0200

    cleanup tests

--- test_aws_keys.py
+++ test_aws_keys.py
@@ -1,5 +1,5 @@
 import pytest
-from aws_keys import md5_fingerprint, sha1_fingerprint, fp_format, fingerprint
+from aws_keys import fp_format, fingerprint
 
 def test_fp_format():
     in_str = "12345"
@@ -7,12 +7,12 @@ def test_fp_format():
     assert fp_format(in_str) == out_str
 
 
-def test_md5_fingerprint():
+def test_local_fingerprint():
     proper_fp = "65:8f:82:a0:8f:53:77:ad:63:c8:f7:15:d6:36:69:04"
     with open('locally-created-key') as key_file_obj:
         assert fingerprint(key_file_obj, origin='local') == proper_fp
 
-def test_sha1_fingerprint():
+def test_aws_fingerprint():
     proper_fp = "f7:35:02:f7:1e:f4:7c:e7:16:7b:f3:98:dc:f9:16:d6:d2:fd:13:21"
     with open('aws-created-key') as key_file_obj:
         assert fingerprint(key_file_obj, origin='aws') == proper_fp
